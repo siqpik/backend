f6379466ab8047c8da6c3a797abaeef34e6f0b60
Wepale (Yeray Rodriguez) <wepale17@gmail.com>
2019-05-26 15:42:24 +0200

    trying to upload to heroku

---
+++ src/main/java/com/example/siqpik/SiqpikApplication.java
@@ -0,0 +1,23 @@
+package com.example.siqpik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class SiqpikApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello SiqpiK!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiqpikApplication.class, args);
+	}
+
+}
