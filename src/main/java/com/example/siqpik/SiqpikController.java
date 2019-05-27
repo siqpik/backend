ea05c003a0614f9fa209b7bf4cd567c18fef33b0
Wepale (Yeray Rodriguez) <wepale17@gmail.com>
2019-05-27 09:18:38 +0200

    url to upload images added

--- src/main/java/com/example/siqpik/SiqpikController.java
+++ src/main/java/com/example/siqpik/SiqpikController.java
@@ -1,15 +1,30 @@
 package com.example.siqpik;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.Map;
+import com.cloudinary.*;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @RestController
 public class SiqpikController {
+
+    Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+            "cloud_name", "siqpik",
+            "api_key", "437532797296387",
+            "api_secret", "WLHW9vutYNCgmK4hFvPAzja2Sb0"));
 
     @RequestMapping("/members")
     @ResponseBody
     String home() {
         return "Hello we are Ronn, Pancho, Laura and Yeray. AND WE ARE SIQPIK!!";
+    }
+
+    @RequestMapping(value = "/image", method = RequestMethod.POST)
+    public Map<String, Object> signUpPlayer(@RequestParam("file") MultipartFile file) throws IOException {
+        return cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
     }
 }
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,16 @@
+
+const sendPic = async () => {
+    const myForm = new FormData().
+    myForm.append("file", document.getElementById("file").files[0]);
+        try {
+            const response = await fetch('https://siqpik.herokuapp.com/image', {
+                method: 'POST',
+                body: myForm
+            });
+            const data = await response.json();
+            console.log(data);
+
+        } catch (error) {
+            console.log("Error: ", error)
+        }
+};
