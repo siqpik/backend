bb9c85d7aeef0c77f62926df67068e0a31dd213c
Wepale (Yeray Rodriguez) <wepale17@gmail.com>
2019-07-27 13:24:23 +0200

    save photo on server

--- src/main/java/com/example/siqpik/SiqpikApplication.java
+++ src/main/java/com/example/siqpik/SiqpikApplication.java
@@ -18,7 +18,7 @@ public class SiqpikApplication {
 	@Bean
 	public CommandLineRunner initData(UserRepository userRepo) {
 		return (args) -> {
-//
+
 //			User user1 = new User("RDave", "Ronnie Dave", "dave@gnmail.com", "1234");
 //			User user2 = new User("Yery", "Yeray Rodriguez", "yeray@gmail.com", "5678");
 //			User user3 = new User("Captain America", "Pancho Viz", "p.viz@gmail.com", "6789");
