34c31b98cb4880ce95c8445ad732ddae57fd7b64
ronn <ronnie@nextdigital.es>
2019-08-11 00:51:23 +0200

    make BasicAuthController a RestController

--- src/main/java/com/example/siqpik/BasicAuthenticationController.java
+++ src/main/java/com/example/siqpik/BasicAuthenticationController.java
@@ -2,10 +2,12 @@ package com.example.siqpik;
 
 import com.example.siqpik.auth.AuthenticationBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class BasicAuthenticationController {
 
-    @GetMapping(path = "/basicauth")
+    @GetMapping("/basicauth")
     public AuthenticationBean authenticate() {
         //throw new RuntimeException("Some Error has Happened! Contact Support at ***-***");
         return new AuthenticationBean("You are authenticated");
