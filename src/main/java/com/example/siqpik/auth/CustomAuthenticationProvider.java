09a2d6728623b6e4017a2f3f6f7ff8168ca43763
ronn <ronnie@nextdigital.es>
2019-08-11 18:09:55 +0200

    Delete default user and pass

---
+++ src/main/java/com/example/siqpik/auth/CustomAuthenticationProvider.java
@@ -0,0 +1,42 @@
+import com.example.siqpik.User;
+import com.example.siqpik.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserService userService;
+
+    @Autowired
+    public CustomAuthenticationProvider(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        return userService.getUser(authentication)
+                .map(user -> {
+                    /*List<GrantedAuthority> authorities = new ArrayList<>();
+                    authorities.add(new SimpleGrantedAuthority("USER")); // description is a string*/
+
+                    return new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword(), null);
+                }).orElse(null);
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
