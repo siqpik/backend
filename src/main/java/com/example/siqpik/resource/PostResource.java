65a57832797bf603cb10c681b096aefb1937facb
ronn <ronnie@nextdigital.es>
2019-09-28 19:26:19 +0200

    Ad feature tu get Posts

--- src/main/java/com/example/siqpik/resource/PostResource.java
+++ src/main/java/com/example/siqpik/resource/PostResource.java
@@ -1,4 +1,29 @@
 package com.example.siqpik.resource;
 
+import com.example.siqpik.resource.model.PostResult;
+import com.example.siqpik.service.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Service
+@RestController
+@RequestMapping("/api/posts")
 public class PostResource {
+
+    private final PostService postService;
+
+    public PostResource(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostResult>> getPosts(Authentication auth){
+        return ResponseEntity.ok(postService.getPosts(auth));
+    }
 }
--- src/main/java/com/example/siqpik/resource/model/PostResult.java
+++ src/main/java/com/example/siqpik/resource/model/PostResult.java
@@ -1,4 +1,28 @@
 package com.example.siqpik.resource.model;
 
+import com.example.siqpik.dto.PhotoDto;
+
 public class PostResult {
+
+    private final String profilePicUrl;
+    private final String userName;
+    private final PhotoDto photo;
+
+    public PostResult(String profilePicUrl, String userName, PhotoDto photo) {
+        this.profilePicUrl = profilePicUrl;
+        this.userName = userName;
+        this.photo = photo;
+    }
+
+    public String getProfilePicUrl() {
+        return profilePicUrl;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public PhotoDto getPhoto() {
+        return photo;
+    }
 }
