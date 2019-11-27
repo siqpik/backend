a5a309df173f85aa1348105edbc3e947fb67c7f5
Yeray R <wepale17@gmail.com>
2019-11-27 10:14:42 +0100

    Fix URI

--- src/main/java/com/example/siqpik/resource/CameraResource.java
+++ src/main/java/com/example/siqpik/resource/CameraResource.java
@@ -1,10 +1,8 @@
 package com.example.siqpik.resource;
 
-
 import com.example.siqpik.dto.AttemptedPicsDto;
 import com.example.siqpik.service.PhotoService;
 import com.example.siqpik.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -14,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api)")
+@RequestMapping("/api")
 public class CameraResource {
 
     private final UserService userService;
