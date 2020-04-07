5f5f36c4e69c556c945006bf450fdfc076fed296
ronn <ronnie@nextdigital.es>
2020-04-07 16:32:05 +0200

    Tests for GenericHelper

--- src/test/java/com/example/siqpik/util/GenericHelperTest.java
+++ src/test/java/com/example/siqpik/util/GenericHelperTest.java
@@ -1,16 +1,35 @@
 package com.example.siqpik.util;
 
-import com.example.siqpik.domain.User;
+import org.junit.Test;
 
-public class GenericHelper {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-  private Boolean requestIsValid(User user){
-    return isNotNullNorEmpty(user.getUserName()) &&
-        isNotNullNorEmpty(user.getPassword()) &&
-        isNotNullNorEmpty(user.getEmail());
+public class GenericHelperTest {
+
+  @Test
+  public void testIsNotNullNorEmpty() {
+    String case1 = null;
+    String case2 = "";
+    String case3 = "Hallo";
+
+    assertFalse(GenericHelper.isNotNullNorEmpty(case2));
+    assertFalse(GenericHelper.isNotNullNorEmpty(case1));
+    assertTrue(GenericHelper.isNotNullNorEmpty(case3));
   }
 
-  protected static boolean isNotNullNorEmpty(String str) {
-    return str != null && !str.isEmpty();
+  @Test
+  public void testAreNotNullNorEmpty() {
+    String[] strs = {"", ""};
+    assertFalse(GenericHelper.areNotNullNorEmpty(strs));
+
+    String[] strs2 = {"2", ""};
+    assertFalse(GenericHelper.areNotNullNorEmpty(strs2));
+
+    String[] strs3 = {"2", "3"};
+    assertTrue(GenericHelper.areNotNullNorEmpty(strs3));
+
+    String[] strs4 = {"2", null};
+    assertFalse(GenericHelper.areNotNullNorEmpty(strs4));
   }
 }
