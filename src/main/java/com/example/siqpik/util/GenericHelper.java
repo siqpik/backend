81ec950b512505113b58611f2af7fdf16e5d3815
ronn <ronnie@nextdigital.es>
2020-04-07 16:31:36 +0200

    Add Helper for generic functions

--- src/main/java/com/example/siqpik/util/GenericHelper.java
+++ src/main/java/com/example/siqpik/util/GenericHelper.java
@@ -1,4 +1,15 @@
 package com.example.siqpik.util;
 
+import java.util.stream.Stream;
+
 public class GenericHelper {
+
+  public static boolean isNotNullNorEmpty(String str) {
+    return str != null && !str.isEmpty();
+  }
+
+  public static boolean areNotNullNorEmpty(String... strs) {
+    return Stream.of(strs)
+        .allMatch(GenericHelper::isNotNullNorEmpty);
+  }
 }
