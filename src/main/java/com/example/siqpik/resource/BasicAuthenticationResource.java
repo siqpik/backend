fe6f651ac391432d90b9a60094daf3ab69ffd7c4
ronn <ronnie@nextdigital.es>
2020-04-07 16:42:38 +0200

    Fix signup functionallity

--- src/main/java/com/example/siqpik/resource/BasicAuthenticationResource.java
+++ src/main/java/com/example/siqpik/resource/BasicAuthenticationResource.java
@@ -2,36 +2,50 @@ package com.example.siqpik.resource;
 
 import com.example.siqpik.domain.User;
 import com.example.siqpik.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.siqpik.util.GenericHelper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class BasicAuthenticationResource {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    /*@GetMapping("/basicauth")
-    public AuthenticationBean authenticate() {
-        //throw new RuntimeException("Some Error has Happened! Contact Support at ***-***");
-        return new AuthenticationBean("You are authenticated");
-    }*/
-    @PostMapping("/register")
-    public ResponseEntity createUser(@RequestBody User user) {
-        return user.getUserName().isEmpty() || user.getPassword().isEmpty()
-                ? ResponseEntity.status(403).build()
-                : userService.getUserRepo().findByUserName(user.getUserName())
-                .map(user1 -> ResponseEntity.status(409).build())
-                .orElseGet(() -> {
-                    user.setPassword(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(user.getPassword()));
-                    userService.getUserRepo().save(user);
-                    return new ResponseEntity<>(HttpStatus.CREATED);
-                });
+  public BasicAuthenticationResource(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PostMapping("/register")
+  public ResponseEntity<Object> createUser(@RequestBody User user) {
+    if (!requestIsValid(user)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
+
+    if (userService.existsByEmail(user.getEmail())){
+      return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already exists");
+    }
+
+    if (userService.existsByUserName(user.getUserName())){
+      return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists");
+    }
+
+    user.setPassword(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(user.getPassword()));
+    userService.save(user);
+
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  private Boolean requestIsValid(User user){
+    return GenericHelper.areNotNullNorEmpty(
+        user.getUserName(),
+        user.getPassword(),
+        user.getEmail()
+    );
+  }
 }
